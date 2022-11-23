d1f92b0ad60d59e0dae472fb2283c6be1145770c
powcoder <powcoder@163.com>
2022-11-23 13:12:55 +0800

    TextAnalysis

---
+++ main_a3.py
@@ -0,0 +1,26 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+"""main_3a.py
+
+An instance of the Text class should be initialized with a file path (a file or
+directory). The example here uses object as the super class, but you may use
+nltk.text.Text as the super class.
+
+An instance of the Vocabulary class should be initialized with an instance of
+Text (not nltk.text.Text).
+
+"""
+
+
+class Text(object):
+
+    def __init__(self, path):
+        pass
+
+
+class Vocabulary(object):
+
+    def __init__(self, text):
+        pass
